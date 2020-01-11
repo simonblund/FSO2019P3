7ee01e016f529651507a90c171b9dc144c97ffe3
Simon Blomsterlund <simon@simonblund.com>
2020-01-11 23:47:15 +0200

    testing adding ui

---
+++ build/precache-manifest.d10b36e1d229647840cf1632b1254b04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496b24c2d43aa85e8d6de656e3cdd14c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01336ee56e58df3e9d55",
+    "url": "/static/css/main.0259e5d2.chunk.css"
+  },
+  {
+    "revision": "6a19a0beea039a280f26",
+    "url": "/static/js/2.3c62b441.chunk.js"
+  },
+  {
+    "revision": "01336ee56e58df3e9d55",
+    "url": "/static/js/main.f7266548.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
