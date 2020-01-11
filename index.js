fc901decc8e39846d0f9ad28179f5bfecd3bf307
Simon Blomsterlund <simon@simonblund.com>
2020-01-11 17:40:06 +0200

    Check 1101

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello World')
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123123123",
+    id: 1
+  },
+  {
+    name: "Maaret Hellas",
+    number: "040-121212",
+    id: 2
+  },
+
+]
+
+app.get('/api/persons', (req,res) => {
+  res.json(persons)
 })
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+app.get('/index', (req,res) => {
+  res.send('<p>Phonebook contains the info of '+persons.length+' persons</p><br><p>'+req.headers.date+'</p>')
+  
+})
+
+
+const PORT =3001
+
+app.listen(PORT, () =>{
+  console.log('Server running on ${PORT}')
+})
